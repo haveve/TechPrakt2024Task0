cbc2692d97c9a6341638428ae6d73063caa02c29
IvanPogoidash <ipz224_pis@student.ztu.edu.ua>
2024-01-27 20:43:02 +0200

    Net project that count sum of two numbers was created

---
+++ SumTwoNumbers/Program.cs
@@ -0,0 +1,11 @@
+﻿try
+{
+	Console.Write("First number: ");
+	double num1 = double.Parse(Console.ReadLine());
+	Console.Write("Second number: ");
+	double num2 = double.Parse(Console.ReadLine());
+	Console.WriteLine("{1} + {2} = {0}",num1+num2,num1,num2);
+}
+catch{
+	Console.WriteLine("One of the numers are incorrect");
+}
